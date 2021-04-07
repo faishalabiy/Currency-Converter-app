f0c146e4f02a84af481ee6420fe71c5469ae11df
Faishal <36927436+faishalabiy@users.noreply.github.com>
2021-04-07 09:49:37 +0700

    Add files via upload

---
+++ Currency Converter App/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Currency_Converter_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCurrency = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromCurr = new System.Windows.Forms.ComboBox();
+            this.toCurr = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert Value:";
+            // 
+            // txtCurrency
+            // 
+            this.txtCurrency.Location = new System.Drawing.Point(15, 26);
+            this.txtCurrency.Name = "txtCurrency";
+            this.txtCurrency.Size = new System.Drawing.Size(111, 20);
+            this.txtCurrency.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Currency option:";
+            // 
+            // fromCurr
+            // 
+            this.fromCurr.FormattingEnabled = true;
+            this.fromCurr.Location = new System.Drawing.Point(15, 110);
+            this.fromCurr.Name = "fromCurr";
+            this.fromCurr.Size = new System.Drawing.Size(63, 21);
+            this.fromCurr.TabIndex = 5;
+            // 
+            // toCurr
+            // 
+            this.toCurr.FormattingEnabled = true;
+            this.toCurr.Location = new System.Drawing.Point(84, 110);
+            this.toCurr.Name = "toCurr";
+            this.toCurr.Size = new System.Drawing.Size(63, 21);
+            this.toCurr.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "To:";
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Location = new System.Drawing.Point(12, 169);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(212, 42);
+            this.ConvertBtn.TabIndex = 9;
+            this.ConvertBtn.Text = "CONVERT!";
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result:";
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.result.Location = new System.Drawing.Point(55, 236);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(106, 32);
+            this.result.TabIndex = 11;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(132, 26);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(92, 20);
+            this.ClearBtn.TabIndex = 12;
+            this.ClearBtn.Text = "clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 325);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ConvertBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.toCurr);
+            this.Controls.Add(this.fromCurr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCurrency);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Currency Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCurrency;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox fromCurr;
+        private System.Windows.Forms.ComboBox toCurr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ Currency Converter App/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Currency_Converter_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BindCurrency();
+        }
+
+        private double getRate(string fromCurrency, string toCurrency)
+        {
+            var json = "";
+            double rate;
+            try
+            {
+                string url = string.Format("https://free.currconv.com/api/v7/convert?q={0}_{1}&compact=ultra&apiKey=cd0304f3d4c6b7e923d3", fromCurrency.ToUpper(), toCurrency.ToUpper());
+                string key = string.Format("{0}_{1}", fromCurrency.ToUpper(), toCurrency.ToUpper());
+
+                json = new WebClient().DownloadString(url);
+                dynamic stuff = JsonConvert.DeserializeObject(json);
+                rate = stuff[key];
+            }
+            catch
+            {
+                rate = 0;
+            }
+            return rate;
+        }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurr = new DataTable();
+            dtCurr.Columns.Add("Text");
+
+            dtCurr.Rows.Add("--SELECT--");
+            dtCurr.Rows.Add("IDR");
+            dtCurr.Rows.Add("INR");
+            dtCurr.Rows.Add("USD");
+            dtCurr.Rows.Add("EUR");
+            dtCurr.Rows.Add("SAR");
+            dtCurr.Rows.Add("BAM");
+            dtCurr.Rows.Add("PYG");
+            dtCurr.Rows.Add("TRY");
+            dtCurr.Rows.Add("JPY");
+
+            fromCurr.DataSource = dtCurr.DefaultView;
+            fromCurr.DisplayMember = "Text";
+            fromCurr.SelectedIndex = 0;
+
+            toCurr.DataSource = dtCurr.DefaultView;
+            toCurr.DisplayMember = "Text";
+            toCurr.SelectedIndex = 0;
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            double ConvertedValue;
+
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency");
+                txtCurrency.Focus();
+                return;
+            }
+
+            if (fromCurr.Text == toCurr.Text)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                this.result.Text = ConvertedValue.ToString();
+            }
+
+            else
+            {
+                double rate = getRate(fromCurr.Text, toCurr.Text);
+                ConvertedValue = (rate * double.Parse(txtCurrency.Text));
+                this.result.Text = ConvertedValue.ToString();
+            }
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            txtCurrency.Clear();
+            result.Text = "";
+            toCurr.SelectedIndex = 0;
+            fromCurr.SelectedIndex = 0;
+        }
+    }
+    
+}
